aeb1d1f35ac906d67b815324eb5880456a377936
Jan Telinger <telingerj@skola.betlemska.cz>
2026-03-06 09:34:20 +0100

    edit lesson 08

---
+++ 08_dedicnost/rpg.py
@@ -0,0 +1,87 @@
+#  hra, ve které bojují dvě armády proti sobě
+import random
+
+
+
+class Armada:
+    def __init__(self, jmeno, barva):
+        self.jmeno = jmeno
+        self.barva = barva
+        self.postavy = []
+
+
+    def pridej_postavu(self, postava):
+        if not isinstance(postava, Postava):
+            raise ValueError("postava není typu Postava")
+        self.postavy.append(postava)
+        postava.pridej_armadu(self)
+
+
+class Postava:
+    def __init__(self, jmeno, zivot):
+        self.jmeno = jmeno
+        self.zivot = zivot
+        self.armada = None
+
+
+    def pridej_armadu(self, armada):
+        self.armada = armada
+
+
+    def uber_zivot(self, zivoty):
+        self.zivot -= zivoty
+
+
+class Bojovnik(Postava):
+    def __init__(self, jmeno, zivot, poskozeni):
+        super().__init__(jmeno, zivot)
+        self.poskozeni = poskozeni
+
+
+    def utok(self, postava):
+        pass
+
+
+class Lucistnik(Bojovnik):
+    def __init__(self, jmeno, zivot, poskozeni, pocet_sipu):
+        super().__init__(jmeno, zivot, poskozeni)
+        self.pocet_sipu = pocet_sipu
+
+
+    def utok(self, postava):
+        self.pocet_sipu -= 1
+        postava.uber_zivot(self.poskozeni)
+
+
+class Sermir(Bojovnik):
+    def __init__(self, jmeno, zivot, poskozeni, ucinnost_stitu):
+        super().__init__(jmeno, zivot, poskozeni)
+        self.ucinnost_stitu = ucinnost_stitu
+
+
+    def utok(self, postava):
+        postava.uber_zivot(self.poskozeni)
+
+
+    def uber_zivot(self, zivoty):
+        if random.randint(1, 100) <= self.ucinnost_stitu:
+            self.ucinnost_stitu *= 0.9
+            return
+        self.zivot -= zivoty
+
+
+class Kouzelnik(Postava):
+    def __init__(self, jmeno, zivot, uzdraveni):
+        super().__init__(jmeno, zivot)
+        self.uzdraveni = uzdraveni
+
+
+    def uzdrav(self, postava):
+        postava.zivot += self.uzdraveni
+
+
+hodni = Armada("hodni", "modra")
+zli = Armada("zli", "cervena")
+
+p1 = Postava("Pepa", 100)
+hodni.pridej_postavu(p1)
