0665a9e5a0b254c5cabc62bfd4b15fed5212f883
Jan Telinger <telingerj@skola.betlemska.cz>
2026-03-27 09:34:20 +0100

    update lesson 08

--- 08_dedicnost/rpg.py
+++ 08_dedicnost/rpg.py
@@ -1,6 +1,8 @@
 #  hra, ve které bojují dvě armády proti sobě
 import random
+import pygame
 
+pygame.init()
 
 
 class Armada:
@@ -18,9 +20,10 @@ class Armada:
 
 
 class Postava:
-    def __init__(self, jmeno, zivot):
+    def __init__(self, jmeno, zivot, pozice):
         self.jmeno = jmeno
         self.zivot = zivot
+        self.pozice = pozice
         self.armada = None
 
 
@@ -33,8 +36,8 @@ class Postava:
 
 
 class Bojovnik(Postava):
-    def __init__(self, jmeno, zivot, poskozeni):
-        super().__init__(jmeno, zivot)
+    def __init__(self, jmeno, zivot, pozice, poskozeni):
+        super().__init__(jmeno, zivot, pozice)
         self.poskozeni = poskozeni
 
 
@@ -43,8 +46,8 @@ class Bojovnik(Postava):
 
 
 class Lucistnik(Bojovnik):
-    def __init__(self, jmeno, zivot, poskozeni, pocet_sipu):
-        super().__init__(jmeno, zivot, poskozeni)
+    def __init__(self, jmeno, zivot, pozice, poskozeni, pocet_sipu):
+        super().__init__(jmeno, zivot, pozice, poskozeni)
         self.pocet_sipu = pocet_sipu
 
 
@@ -54,8 +57,8 @@ class Lucistnik(Bojovnik):
 
 
 class Sermir(Bojovnik):
-    def __init__(self, jmeno, zivot, poskozeni, ucinnost_stitu):
-        super().__init__(jmeno, zivot, poskozeni)
+    def __init__(self, jmeno, zivot, pozice, poskozeni, ucinnost_stitu):
+        super().__init__(jmeno, zivot, pozice, poskozeni)
         self.ucinnost_stitu = ucinnost_stitu
 
 
@@ -71,17 +74,30 @@ class Sermir(Bojovnik):
 
 
 class Kouzelnik(Postava):
-    def __init__(self, jmeno, zivot, uzdraveni):
-        super().__init__(jmeno, zivot)
+    def __init__(self, jmeno, zivot, pozice, uzdraveni):
+        super().__init__(jmeno, zivot, pozice)
         self.uzdraveni = uzdraveni
 
 
     def uzdrav(self, postava):
         postava.zivot += self.uzdraveni
 
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((800, 800))
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-hodni = Armada("hodni", "modra")
-zli = Armada("zli", "cervena")
+    def loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-p1 = Postava("Pepa", 100)
-hodni.pridej_postavu(p1)
+            self.screen.fill((255, 255, 255))
+            self.clock.tick(60)
+            pygame.display.flip()
+
+
+game = Game()
+game.loop()
