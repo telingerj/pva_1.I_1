937058c4b1cde7d879b913f86b14be2f02ad945b
Jan Telinger <telingerj@skola.betlemska.cz>
2026-04-17 09:32:32 +0200

    update lesson 08

--- 08_dedicnost/rpg.py
+++ 08_dedicnost/rpg.py
@@ -20,11 +20,15 @@ class Armada:
 
 
 class Postava:
-    def __init__(self, jmeno, zivot, pozice):
+    def __init__(self, jmeno, zivot, pozice, textura_leva, textura_prava):
         self.jmeno = jmeno
         self.zivot = zivot
+        self.max_zivot = zivot
         self.pozice = pozice
         self.armada = None
+        self.textura_leva = textura_leva
+        self.textura_prava = textura_prava
+        self.otoceni = True  # výchozí otočení - doleva
 
 
     def pridej_armadu(self, armada):
@@ -35,9 +39,37 @@ class Postava:
         self.zivot -= zivoty
 
 
+    def otoc(self):
+        self.otoceni = not self.otoceni
+
+
+    def vykresli(self, screen):
+        if self.otoceni:
+            screen.blit(self.textura_leva, self.pozice)
+        else:
+            screen.blit(self.textura_prava, self.pozice)
+
+        pomer_zivotu = self.zivot / self.max_zivot
+
+        pygame.draw.rect(screen, (100, 100, 100),
+                         (self.pozice[0] - 5, self.pozice[1] - 15, 30, 5))
+        pygame.draw.rect(screen, (255, 0, 0),
+                         (self.pozice[0] - 5, self.pozice[1] - 15, 30 * pomer_zivotu, 5))
+
+
+    def pohyb(self):
+        if self.otoceni:
+            self.pozice = (self.pozice[0] - 1, self.pozice[1])
+        else:
+            self.pozice = (self.pozice[0] + 1, self.pozice[1])
+
+        #TODO: otočení postavy když dorazí na kraj okna
+
+
+
 class Bojovnik(Postava):
-    def __init__(self, jmeno, zivot, pozice, poskozeni):
-        super().__init__(jmeno, zivot, pozice)
+    def __init__(self, jmeno, zivot, pozice, poskozeni, textura_leva, textura_prava):
+        super().__init__(jmeno, zivot, pozice, textura_leva, textura_prava)
         self.poskozeni = poskozeni
 
 
@@ -46,8 +78,8 @@ class Bojovnik(Postava):
 
 
 class Lucistnik(Bojovnik):
-    def __init__(self, jmeno, zivot, pozice, poskozeni, pocet_sipu):
-        super().__init__(jmeno, zivot, pozice, poskozeni)
+    def __init__(self, jmeno, zivot, pozice, poskozeni, pocet_sipu, textura_leva, textura_prava):
+        super().__init__(jmeno, zivot, pozice, poskozeni, textura_leva, textura_prava)
         self.pocet_sipu = pocet_sipu
 
 
@@ -57,8 +89,8 @@ class Lucistnik(Bojovnik):
 
 
 class Sermir(Bojovnik):
-    def __init__(self, jmeno, zivot, pozice, poskozeni, ucinnost_stitu):
-        super().__init__(jmeno, zivot, pozice, poskozeni)
+    def __init__(self, jmeno, zivot, pozice, poskozeni, ucinnost_stitu, textura_leva, textura_prava):
+        super().__init__(jmeno, zivot, pozice, poskozeni, textura_leva, textura_prava)
         self.ucinnost_stitu = ucinnost_stitu
 
 
@@ -74,8 +106,8 @@ class Sermir(Bojovnik):
 
 
 class Kouzelnik(Postava):
-    def __init__(self, jmeno, zivot, pozice, uzdraveni):
-        super().__init__(jmeno, zivot, pozice)
+    def __init__(self, jmeno, zivot, pozice, uzdraveni, textura_leva, textura_prava):
+        super().__init__(jmeno, zivot, pozice, textura_leva, textura_prava)
         self.uzdraveni = uzdraveni
 
 
@@ -87,6 +119,55 @@ class Game:
         self.screen = pygame.display.set_mode((800, 800))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.armady = []
+        self.textury = []
+        self.nacti_textury()
+        self.vytvor_armady()
+
+
+
+    def vytvor_armady(self):
+        a1 = Armada("hodni", (0, 0, 255))
+        a2 = Armada("zli", (255, 0, 0))
+
+        s1 = Sermir("Pepa", 100, (100, 100), 10, 10, self.textury[2], self.textury[3])
+        l1 = Lucistnik("Franta", 80, (80, 250), 10, 10, self.textury[0], self.textury[1])
+        k1 = Kouzelnik("Gandalf", 100, (100, 500), 5, self.textury[4], self.textury[5])
+
+        s2 = Sermir("Jakub", 120, (600, 100), 8, 5, self.textury[2], self.textury[3])
+        l2 = Lucistnik("Honza", 60, (620, 250), 10, 15, self.textury[0], self.textury[1])
+        k2 = Kouzelnik("Merlin", 100, (600, 500), 5, self.textury[4], self.textury[5])
+
+        a1.pridej_postavu(s1)
+        a1.pridej_postavu(l1)
+        a1.pridej_postavu(k1)
+
+        a2.pridej_postavu(s2)
+        a2.pridej_postavu(l2)
+        a2.pridej_postavu(k2)
+
+
+        for p in a1.postavy:
+            p.otoc()
+
+        self.armady = [a1, a2]
+
+
+    def nacti_textury(self):
+        #textura = pygame.image.load("images/archer_left.png")
+
+        for jmeno_postavy in ["archer", "swordsman", "magician"]:
+            for otoceni in ["left", "right"]:
+                textura = pygame.image.load("images/" + jmeno_postavy + "_" + otoceni + ".png")
+                self.textury.append(textura)
+
+
+    def vykresli(self):
+        for a in self.armady:
+            for p in a.postavy:
+                p.vykresli(self.screen)
+                p.pohyb()
+
 
     def loop(self):
         while self.running:
@@ -95,6 +176,7 @@ class Game:
                     self.running = False
 
             self.screen.fill((255, 255, 255))
+            self.vykresli()
             self.clock.tick(60)
             pygame.display.flip()
 
