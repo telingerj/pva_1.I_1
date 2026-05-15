61f89e9ae6a08f10e388c42e84753c06d89bd7b3
Jan Telinger <telingerj@skola.betlemska.cz>
2026-05-15 09:29:42 +0200

    add utils

--- zaverecne_projekty/utils/soubory.py
+++ zaverecne_projekty/utils/soubory.py
@@ -5,4 +5,7 @@ seznam = obsah.split("\n")  # rozdělení řádků do seznamu
 soucet = 0
 for i in seznam:
     soucet += int(i)
-print(soucet / len(seznam))
+prumer = soucet / len(seznam)
+file = open("text_files/soubor2.txt", "w", encoding="utf-8")  # otevření jiného textového souboru pro zápis (w)
+file.write(str(prumer))
+file.close()
